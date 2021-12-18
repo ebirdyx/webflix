165ae4d84f4f129e4069ca1c1a5bcc75be042683
Pamela Brahollari <ebirdyxx@gmail.com>
2021-12-18 20:46:50 +0100

    added spring boot project

---
+++ src/main/java/al/edu/cit/webflix/WebflixApplication.java
@@ -0,0 +1,13 @@
+package al.edu.cit.webflix;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebflixApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebflixApplication.class, args);
+	}
+
+}
---
+++ src/test/java/al/edu/cit/webflix/WebflixApplicationTests.java
@@ -0,0 +1,13 @@
+package al.edu.cit.webflix;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebflixApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
