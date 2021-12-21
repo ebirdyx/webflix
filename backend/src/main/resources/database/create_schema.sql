ac26c80651e7c8576edecb8677dc99ce76592262
Pamela Brahollari <ebirdyxx@gmail.com>
2021-12-21 17:22:30 +0100

    append drop tables to create schema file

--- backend/src/main/resources/database/create_schema.sql
+++ backend/src/main/resources/database/create_schema.sql
@@ -1,3 +1,30 @@
+-- drop all tables --
+set foreign_key_checks = 0;
+
+drop table if exists Address;
+drop table if exists User;
+drop table if exists CreditCard;
+drop table if exists PersonRolePlayed;
+drop table if exists Customer;
+drop table if exists CustomerSubscription;
+drop table if exists MovieDVD;
+drop table if exists Trailer;
+drop table if exists SubscriptionPayment;
+drop table if exists Employee;
+drop table if exists MovieGenre;
+drop table if exists Rentals;
+drop table if exists People;
+drop table if exists Genre;
+drop table if exists MovieLanguages;
+drop table if exists MovieProductionCountry;
+drop table if exists ProductionCountry;
+drop table if exists Scriptwriter;
+drop table if exists Movie;
+drop table if exists Subscription;
+
+set foreign_key_checks = 1;
+
+-- create schema --
 create table Address
 (
     id        int primary key auto_increment not null,
