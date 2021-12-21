7d287ebae37451b1657ca9224c9774a5e1280b1e
Pamela Brahollari <ebirdyxx@gmail.com>
2021-12-21 17:19:56 +0100

    Add configuration for spring to redirect views in react

---
+++ backend/src/main/java/al/edu/cit/webflix/configuration/WebConfiguration.java
@@ -0,0 +1,20 @@
+package al.edu.cit.webflix.configuration;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfiguration implements WebMvcConfigurer {
+
+    // Suppressed view inspection due to index.html being copied from frontend by using the maven-resources-plugin
+    @SuppressWarnings("SpringMVCViewInspection")
+    @Override
+    public void addViewControllers(ViewControllerRegistry registry) {
+        registry.addViewController("/").setViewName("forward:/index.html");
+        registry.addViewController("/{x:[\\w\\-]+}").setViewName("forward:/index.html");
+        registry.addViewController("/{x:^(?!api$).*$}/**/{y:[\\w\\-]+}").setViewName("forward:/index.html");
+        registry.addViewController("/swagger-ui").setViewName("redirect:/swagger-ui/");
+        registry.addViewController("/swagger-ui/").setViewName("forward:/swagger-ui/index.html");
+    }
+}
