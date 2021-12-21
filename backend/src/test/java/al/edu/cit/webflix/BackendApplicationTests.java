4cfbcfa96d22b7be1e050a7be9cf97df0c628dda
Pamela Brahollari <ebirdyxx@gmail.com>
2021-12-21 16:31:14 +0100

    create frontend and backend projects

---
+++ backend/src/test/java/al/edu/cit/webflix/BackendApplicationTests.java
@@ -0,0 +1,13 @@
+package al.edu.cit.webflix;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
