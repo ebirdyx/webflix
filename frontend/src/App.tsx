df3a5e2f2f08010be6849cad26470ea9272acbbc
Pamela Brahollari <ebirdyxx@gmail.com>
2022-01-28 10:32:43 -0500

    Create navigation bar

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,9 +1,18 @@
 import React from 'react';
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import Navigation from "./components/Navigation";
+import Home from "./components/Home";
 
 function App() {
   return (
-    <div className="App">
-      It works!
+    <div>
+      <BrowserRouter>
+        <Navigation />
+
+        <Routes>
+          <Route path="/" element={<Home />} />
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
---
+++ frontend/src/components/Navigation.tsx
@@ -0,0 +1,22 @@
+import React from "react";
+import {Container, Nav, Navbar, NavDropdown} from "react-bootstrap";
+
+const Navigation = () => {
+  return (
+    <Navbar bg="light" expand="lg">
+      <Container>
+        <Navbar.Brand href="#home">Webflix</Navbar.Brand>
+
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse id="basic-navbar-nav">
+          <Nav className="me-auto">
+            <Nav.Link href="#home">Home</Nav.Link>
+            <Nav.Link href="#link">Link</Nav.Link>
+          </Nav>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
+  )
+}
+
+export default Navigation;
